f99dcb39c8d82a90d01b8a495dffed30fc2fc613
raebaexxx <vadimfeda@yandex.ru>
2025-09-15 18:24:47 +0300

    Add libshim Dolby to prevent Dolby crash

--- extract-files.py
+++ extract-files.py
@@ -95,6 +95,11 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
     (
+        'vendor/bin/hw/vendor.dolby.media.c2@1.0-service', 
+        'vendor/bin/hw/dolbycodec2',
+    ): blob_fixup()
+        .add_needed('libshim_dolby.so'),    
+    (
         'vendor/etc/media_codecs_kalama.xml',
         'vendor/etc/media_codecs_kalama_vendor.xml',
     ): blob_fixup()

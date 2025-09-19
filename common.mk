7ec092261f7baa41ac596395e7054dd6d4b5c270
raebaexxx <vadimfeda@yandex.ru>
2025-09-19 20:06:18 +0300

    Implement torch light control [RealJohnGalt: Modify for oneplus usage and add toggle switch] [cyberknight777/ralph950412: Adapt nodes and SEPolicy for Xiaomi SM8350] [Adapt nodes/sepolicy for Xiaomi SM8550]

--- common.mk
+++ common.mk
@@ -128,6 +128,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery
 
 # Camera
+$(call soong_config_set,libcameraservice,ext_lib,libcameraservice_extension.xiaomi_sm8550)
 $(call soong_config_set,camera,override_format_from_reserved,true)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \

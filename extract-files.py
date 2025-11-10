46d3afc98bd31088af9ce185d20a070772b57bc1
raebaexxx <vadimfeda@yandex.ru>
2025-11-10 17:27:53 +0300

    Remove software omx codec references These are no longer available as of Android 14 QPR3. The C2 codecs available by default in the swcodec apex will be used instead.

--- extract-files.py
+++ extract-files.py
@@ -117,6 +117,12 @@ blob_fixups: blob_fixups_user_type = {
         'odm/lib64/nfc_nci.nqx.default.hw.so'
     ): blob_fixup()
         .add_needed('libbase_shim.so'),
+    (
+       'vendor/etc/media_codecs_kalama.xml',
+       'vendor/etc/media_codecs_kalama_vendor.xml',
+       'vendor/etc/media_codecs_kalama_vendor_without_dvenc.xml',
+    ): blob_fixup()
+        .regex_replace('.+media_codecs_(google_audio|google_telephony|vendor_audio).+\n', ''),
     'vendor/etc/ueventd.rc' : blob_fixup()
         .add_line_if_missing('\n# Charger\n/sys/class/qcom-battery     night_charging            0660    system  system')
 }  # fmt: skip

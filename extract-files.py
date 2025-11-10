825e7e74f799f7f5e3d7d7cafa8861a198adf0f1
raebaexxx <vadimfeda@yandex.ru>
2025-11-10 17:29:45 +0300

    Remove media_codecs_google_c2* AOSP codecs can be used by default and it is not necessary to include them device side.

--- extract-files.py
+++ extract-files.py
@@ -122,7 +122,7 @@ blob_fixups: blob_fixups_user_type = {
        'vendor/etc/media_codecs_kalama_vendor.xml',
        'vendor/etc/media_codecs_kalama_vendor_without_dvenc.xml',
     ): blob_fixup()
-        .regex_replace('.+media_codecs_(google_audio|google_telephony|vendor_audio).+\n', ''),
+        .regex_replace('.+media_codecs_(google_audio|google_c2|google_telephony|vendor_audio).+\n', ''),
     'vendor/etc/ueventd.rc' : blob_fixup()
         .add_line_if_missing('\n# Charger\n/sys/class/qcom-battery     night_charging            0660    system  system')
 }  # fmt: skip

e7c54c96b8399c3b5869da2d75ac940f21639bef
raebaexxx <vadimfeda@yandex.ru>
2025-11-10 17:31:30 +0300

    Use the common device compatibility matrix from QCOM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -194,7 +194,7 @@ TARGET_SYSTEM_PROP += $(COMMON_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/configs/properties/vendor.prop
 
 # VINTF
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/vintf/framework_matrix.xml \
     $(COMMON_PATH)/configs/vintf/product_framework_matrix.xml

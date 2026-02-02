7cfa3e609532958c2928e6715e82aee0bb426161
raebaexxx <vadimfeda@yandex.ru>
2026-02-03 01:52:37 +0300

    Revert "sm8550-common: sensors: introduce sensor-notifier for nonui handling"
    This reverts commit 1d5eb55bb8074f78736d47d24b5ca8dc06bb9a8b.

--- sensors/main.cpp
+++ sensors/main.cpp
@@ -9,10 +9,7 @@
 #include <android-base/logging.h>
 
 #include "NonUiNotifier.h"
-<<<<<<< HEAD
 #include "AodNotifier.h"
-=======
->>>>>>> eeabe88 (sm8550-common: sensors: introduce sensor-notifier for nonui handling)
 
 int main() {
     sp<ISensorManager> manager = ISensorManager::getService();
@@ -21,12 +18,9 @@ int main() {
         return EXIT_FAILURE;
     }
 
-<<<<<<< HEAD
     std::unique_ptr<AodNotifier> aodNotifier = std::make_unique<AodNotifier>(manager);
     aodNotifier->activate();
     
-=======
->>>>>>> eeabe88 (sm8550-common: sensors: introduce sensor-notifier for nonui handling)
     std::unique_ptr<NonUiNotifier> nonUiNotifier = std::make_unique<NonUiNotifier>(manager);
     nonUiNotifier->activate();
 

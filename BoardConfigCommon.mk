812ed7a32c6074adfa4fc860468349268a14af45
raebaexxx <vadimfeda@yandex.ru>
2026-02-03 01:40:24 +0300

    Import QCC system_ext blobs Makes the QTI GNSS service happy. Thanks luca020400 for the help.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,6 +197,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/vintf/framework_matrix.xml \
     $(COMMON_PATH)/configs/vintf/product_framework_matrix.xml
 
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/configs/vintf/manifest_kalama.xml \
     $(COMMON_PATH)/configs/vintf/manifest_socrates.xml

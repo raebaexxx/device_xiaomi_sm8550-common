102986aa36a06c5b6591dc2c2c5687fcd3192796
raebaexxx <vadimfeda@yandex.ru>
2026-02-03 02:18:29 +0300

    fix \ lol

--- common.mk
+++ common.mk
@@ -319,7 +319,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
-    sensors.dynamic_sensor_hal
+    sensors.dynamic_sensor_hal \
     sensor-notifier \
     sensors.xiaomi.v2
 

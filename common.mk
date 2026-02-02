c2c7e5312545f99b6153ca14bdea804f732a3a02
raebaexxx <vadimfeda@yandex.ru>
2026-02-03 02:06:17 +0300

    Build sensors.xiaomi.v2

--- common.mk
+++ common.mk
@@ -321,6 +321,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
     sensors.dynamic_sensor_hal
     sensor-notifier \
+    sensors.xiaomi.v2
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_ODM)/etc/sensors/hals.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_kalama/android.hardware.sensor.dynamic.head_tracker.xml \

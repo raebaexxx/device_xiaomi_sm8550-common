19312c1ec89d571c3be8fda476a461b30c54f2be
raebaexxx <vadimfeda@yandex.ru>
2026-04-01 03:03:35 +0300

    Revert "big rebuild sources v2"
    This reverts commit 1b7be7f56297a0b6176d8e193954c36718b58d2d.

--- parts/src/com/xiaomi/settings/autohbm/AutoHbmFragment.java
+++ parts/src/com/xiaomi/settings/autohbm/AutoHbmFragment.java
@@ -43,7 +43,7 @@ import com.android.settingslib.widget.UsageProgressBarPreference;
 import com.xiaomi.settings.Constants;
 import com.xiaomi.settings.CustomSeekBarPreference;
 import com.xiaomi.settings.R;
-import com.xiaomi.settings.utils.FileUtils;
+import com.xiaomi.settings.utils.TileUtils;
 
 public class AutoHbmFragment extends PreferenceFragmentCompat
         implements OnCheckedChangeListener, SensorEventListener, Preference.OnPreferenceChangeListener {
@@ -95,7 +95,7 @@ public class AutoHbmFragment extends PreferenceFragmentCompat
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.add_tile) {
-            FileUtils.requestAddTileService(
+            TileUtils.requestAddTileService(
                     getContext(),
                     AutoHbmTileService.class,
                     R.string.auto_hbm_title,

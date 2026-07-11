4b7c62d62008442813e157c6c4f57f395b811845
raebaexxx <vadimfeda@yandex.ru>
2026-07-11 17:49:15 +0300

    Restore fastchg_mode genfs + fix BoardConfig lineage include
    - sepolicy: restore genfscon for /class/qcom-battery/fastchg_mode
      with label sysfs_batteryinfo. Required by BatteryService
      (system_server domain) to read the node and apply
      CrDroid config_oemFastChargerStatusPath for Turbo Charging.
      The rule was removed in 6d722c87 together with xiaomiparts.
    
    - BoardConfigCommon.mk: include vendor/lineage/config/BoardConfigReservedSize.mk
      instead of vendor/infinity/config/BoardConfigReservedSize.mk.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,7 +151,7 @@ $(foreach p, $(call to-upper, $(BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST))
     $(eval TARGET_COPY_OUT_$(p) := $(call to-lower, $(p))))
 
 BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE := false
-include vendor/infinity/config/BoardConfigReservedSize.mk
+include vendor/lineage/config/BoardConfigReservedSize.mk
 
 # Platform
 TARGET_BOARD_PLATFORM := kalama

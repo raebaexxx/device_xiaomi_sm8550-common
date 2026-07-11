d5132a1b3c8bcd9baeb592088484fbdb718c3a2e
raebaexxx <vadimfeda@yandex.ru>
2026-07-11 21:16:33 +0300

    Reapply "remove xiaomiparts v2"
    This reverts commit 02cadba30aff2890811b5a0bb972cd98857e8584.

--- common.mk
+++ common.mk
@@ -140,8 +140,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service
 
-# PRODUCT_PACKAGE_OVERLAPS += DolbyAtmos
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
@@ -294,10 +292,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     LineageResXiaomi
-
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
 
 # Partitions
 PRODUCT_PACKAGES += \
